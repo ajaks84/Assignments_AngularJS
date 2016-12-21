85dffff0c1325b1a1e41a07cd4aaf66ca327eed6
Ajaks84 <x2p@tut.by>
2016-12-21 09:26:46 +0300

    module 2

--- module1-solution/styles/app.js
+++ module1-solution/styles/app.js
@@ -3,17 +3,28 @@
     'use strict'
     angular.module('LunchCheck', [])
         .controller('LunchCheckController', controllerFn);
-
     controllerFn.$inject = ['$scope'];
-
     function controllerFn($scope) {
         $scope.outputString = '';
-
         $scope.checkInput = function() {
             if ($scope.inputString) {
                 var str = $scope.inputString.split(',').filter(Boolean);
-                //console.log(str);
-                if (str.length <= 3) {
+                console.log(str);
+				var emptyEntry=0;
+				console.log("Length is: "+str.length); 
+                for (var i=0; i<str.length; i++){
+				    var pat = new RegExp("\b[ds]", "g");
+				    if (str[i].match(pat)){
+				    	emptyEntry++;
+				    }
+				    
+				}
+                console.log("Empty entry: "+emptyEntry);
+				var result = str.length-emptyEntry;
+
+				console.log(result);
+
+                if (result <= 3) {
                     $scope.outputString = 'Enjoy!';
                 } else {
                     $scope.outputString = 'Too much!';
