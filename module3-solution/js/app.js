bf9c386745be6dead1b53fab79082542b5137fa7
Ajaks84 <x2p@tut.by>
2016-12-28 11:24:06 +0300

    module 3 last revision

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -11,8 +11,7 @@ function FoundItemsDirective(){
     templateUrl: 'found-items.html',
     scope: {
       onRemove: '&',
-      items: '<',
-      message: '<'
+      items: '<'
     }
   };
   return ddo;
@@ -22,47 +21,51 @@ NarrowItDownController.$inject = ['MenuSearchService','$timeout'];
 function NarrowItDownController(MenuSearchService,$timeout) {
   var ctrl = this;
   ctrl.found=[];
-  ctrl.getItems = function (searchTerm) {
-      ctrl.found.length=0;
-      ctrl.message=null;
-      if (!searchTerm==""){
-        ctrl.found=MenuSearchService.getMatchedMenuItems(searchTerm);
-        $timeout(function(){
-          if(ctrl.found.length==0){
-          ctrl.message="Nothing found!";
-          } 
-        },1000);
-      }
-      else ctrl.message="Type something!"; 
-  };
+   ctrl.message=null;
+    ctrl.getItems = function (searchTerm) {
+        if (!searchTerm==""){
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+          promise.then(function(response){
+            ctrl.found = response;
+          console.log(ctrl.found);
+            if (ctrl.found.length == 0) {
+                ctrl.message = "Nothing found"
+            }
+            else {
+                ctrl.message = ""
+            }
+        });
+          }
+           else ctrl.message="Type something!"; 
+
+      };
+
+
   ctrl.removeItem = function (itemIndex) {
     ctrl.found.splice(itemIndex, 1);
-  };
+  }
 }
 
 MenuSearchService.$inject = ['$http'];
 function MenuSearchService($http) {
   var service = this;
-  var foundItems = [];
-  
-  service.getMatchedMenuItems = function (searchTerm) {
-
-// process result and only keep items that match
-
-    var response=$http({
-                        method: "GET",
+   service.getMatchedMenuItems = function (searchTerm) {
+    var foundItems = [];
+          return $http({method: "GET",
                         url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-                        }).then(function (result) {
+                        })
+// process result and only keep items that match
+          .then(function (result) {
                               var re = new RegExp(searchTerm, 'i');
                           for (var i = 0; i < result.data.menu_items.length; i++) {
            if (result.data.menu_items[i].description.match(re)&&searchTerm!=undefined){
             foundItems.push(result.data.menu_items[i]);
                                                       }
                                       }
-                       });
-// return items that match
-      return foundItems;
-            };
+                  // return items that match
+                    return foundItems;     
+    });
+            }
 }
 
 })();
